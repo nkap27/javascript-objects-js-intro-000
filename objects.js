15d62453754bc17462945ddfda5349195a4a0338
kapr.natasha@gmail.com <kapr.natasha@gmail.com>
2018-06-04 20:07:19 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -11,7 +11,6 @@ updatePlaylist(playlist, "Phil Ochs", "There but for Fortune");
 
 function removeFromPlaylist(playlist, artistName) {
   delete playlist.artistName;
-  return playlist;
 }
 
 removeFromPlaylist(playlist, "Slowdive");
