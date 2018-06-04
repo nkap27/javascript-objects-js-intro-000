311724bb1bfcc56f0d9e9ebef914a6c4911c7982
kapr.natasha@gmail.com <kapr.natasha@gmail.com>
2018-06-04 19:49:10 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,5 +2,5 @@ var playlist = {amos: "keep it loose, keep it tight"};
 
 function updatePlaylist(playlist, artistName, songTitle) {
 
-  return Object.assign(playlist, "lee", "violin")
+  return Object.assign({}, playlist, { [artistName]: songTitle })
 }
